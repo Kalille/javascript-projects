cde2db2e90a908ae452e0847bdc64720c77368ec
kalille Overton <>
2024-01-19 13:32:30 -0500

    Studio completed

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -2,10 +2,19 @@ const input = require('readline-sync');
 let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
-//Hint - define another variable to hold the new string or reassign the new string to str.
 
+let modStringOne = str.split("");
+console.log(modStringOne)
+let spliced = modStringOne.splice(0,3).join("")
+modStringOne.push(spliced)
+let joined = modStringOne.join("")
+console.log(joined)
+
+
+// //Hint - define another variable to hold the new string or reassign the new string to str.
+let newStr = joined
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`I took the word ${str} and moved the first 3 letters to the end to make the word \"${joined}\"!`)
+// 2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
-//2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
-
-//3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+// 3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
