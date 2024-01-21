449ada86ea79279d8d5304aedda3f1587db8ec5a
kalille Overton <>
2024-01-21 11:31:11 -0500

    loops execised complete

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -22,3 +22,71 @@ Construct ``for`` loops to accomplish the following tasks:
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+  // exercise 1
+let total = 0
+  for (let i = 0; i < 20; i++){
+    if (i % 2 !== 0){
+      console.log(i)
+    }
+    total += 1
+    // console.log(`${total}`)
+  }
+
+
+  for(let i = 12; i >= -14; i--){
+   if (i % 2 == 0){
+    console.log(i)
+   }
+
+  }
+
+
+  for (i = 50; i > 20; i--){
+    if (i % 3 === 0 ){
+      console.log(i)
+    }
+  }
+
+
+  // exercise 2
+
+  let str = "LaunchCode";
+  let arr = [1, 5, `LC101`, `blue`, 42]
+  
+  let newStr = [];
+  const reversedString=()=>{
+    for (i = 0; i < str.length; i++){
+   
+      newStr.push(str[i])
+   
+    }
+    console.log(newStr.reverse().join(""))
+  }
+
+reversedString()
+  for (i = 0; i < arr.length; i++){
+    
+    console.log(arr[i])
+  }
+
+
+  // exercise 3
+
+  let arr2 = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104]
+
+  let odd = [];
+  let even = [];
+
+  const seperator=()=>{
+    for (i = 0; i < arr2.length; i++){
+      if (i % 2 === 0){
+        even.push(arr2[i])
+      }
+      else odd.push(arr2[i])
+    }
+console.log(even)
+console.log(odd)
+  }
+
+  seperator()
