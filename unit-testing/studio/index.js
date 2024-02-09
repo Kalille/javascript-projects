b2d0f9b277acf397d489c4229234073a4582d019
kalille Overton <>
2024-02-09 12:21:23 -0500

    complete

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,51 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  prgramsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: ()=>{ return " "}
 }
 
-module.exports = launchcode;
+const findKeyInObject=(obj, userInput)=>{
+for (let key in obj){
+  if(key === userInput){
+        return obj[key]
+    }
+}
+}
+// console.log(findKeyInObject(launchcode, "executiveDirector"))
+const updateObjectKey=(obj, objKey,objectKeyValue)=>{
+    for (let key in obj){
+        // console.log(obj.executiveDirector = "Dan")
+        if (key === objKey){
+             console.log(obj.key)
+            obj[key] = objectKeyValue
+        }
+        // console.log(obj)
+  
+    }
+    return obj
+}
+
+const removeKeyValue=(obj, userInput)=>{
+    // console.log(obj)
+    for (let key in obj){
+        if (key === userInput){
+             obj[key] = undefined
+        }
+    }
+    console.log(obj["organization"])
+    return obj
+}
+// console.log(updateObjectKey(launchcode, "executiveDirector", "Dan Orlovsky"))
+console.log(removeKeyValue(launchcode, "organization"))
+
+
+module.exports = {
+    launchcode,
+    updateObjectKey,
+    findKeyInObject,
+    removeKeyValue
+};
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,23 @@
 // launchcode.test.js code:
-const launchcode = require('../index.js');
+const {launchcode,findKeyInObject, updateObjectKey, removeKeyValue} = require('../index.js');
 
 describe("Testing launchcode", function(){
-
-  // Write your unit tests here!
+test("shows that launchcode is a object", ()=>{
+  expect(typeof launchcode).toBe("object")
+});
+test("returns a spefic key in the Launchcode object",()=>{
+  let value = findKeyInObject(launchcode, "executiveDirector")
+  expect(value).toBe("Jeff")
+});
+test("updates executive directors name",()=>{
+  const updatedLaunchCode = updateObjectKey(launchcode, "executiveDirector", "Dan Orlovsky")
+  expect(updatedLaunchCode.executiveDirector).toBe("Dan Orlovsky")
+});
+test("removes a key value and returns undefined for that key",()=>{
+  const removedValue = removeKeyValue(launchcode, "organization")
+      expect(removedValue["organization"]).toBeUndefined()
+    });
+   // Write your unit tests here!
+  
   
 });
