b7b57074434391b3564f2c804bc9c6c9db42162f
kalille Overton <>
2024-03-04 21:35:02 -0500

    complete

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,27 @@
 //TODO: Add Your Code Below
+window.addEventListener('load', function(){
+console.log("load");
+
+let people = 
+
+fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+.then(res=>res.json())
+.then((res=>{
+   const container = document.getElementById("container");
+   
+    res.forEach(data => {
+        console.log(data)
+        const astroDiv = document.createElement('div');
+        astroDiv.classList.add('astronaut');
+        astroDiv.innerHTML = `
+        <h2>${data.firstName} ${data.lastName}</h2>
+        <p>Hours in space: ${data.hoursInSpace}</p>
+        <p>Active: ${data.active}</p>
+        <img src="${data.picture}" alt="${data.firstName} ${data.lastName}">
+    `;
+
+    container.appendChild(astroDiv)
+    })
+
+}))
+})
