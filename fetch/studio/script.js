020fa2c69cfd4a342c303e5666174cd94ba31c16
kalille Overton <>
2024-03-05 09:46:03 -0500

    completed bonus mission

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -9,16 +9,17 @@ fetch("https://handlers.education.launchcode.org/static/astronauts.json")
 .then((res=>{
    const container = document.getElementById("container");
    
-    res.forEach(data => {
+    res.map(data => {
         console.log(data)
         const astroDiv = document.createElement('div');
         astroDiv.classList.add('astronaut');
         astroDiv.innerHTML = `
         <h2>${data.firstName} ${data.lastName}</h2>
         <p>Hours in space: ${data.hoursInSpace}</p>
-        <p>Active: ${data.active}</p>
+        ${data.active ? '<p style="color: green">Active: Yes</p>' : '<p>Active: No</p>'}
         <img style="height: 250px" src="${data.picture}" alt="${data.firstName} ${data.lastName}">
     `;
+
 
     container.appendChild(astroDiv)
     })
